813484f5dabf66395bdff24c07e3835be1b10cfd
reyzalhildha <reyzal.hildha@gmail.com>
2022-01-07 17:37:23 +0700

    I

---
+++ src/main/java/org/acme/InputTextView.java
@@ -0,0 +1,21 @@
+package org.acme;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class InputTextView {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    
+}

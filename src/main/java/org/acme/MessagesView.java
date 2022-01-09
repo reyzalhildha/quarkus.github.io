eb8f7becbcdaf5f2608ebbcee8a85161478daf8b
reyzalhildha <reyzal.hildha@gmail.com>
2022-01-09 20:50:35 +0700

    microapps

---
+++ src/main/java/org/acme/MessagesView.java
@@ -0,0 +1,23 @@
+package org.acme;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class MessagesView {
+
+    public void info() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Message Content"));
+    }
+
+    public void warn() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning", "Message Content"));
+    }
+
+    public void error() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Message Content."));
+    }
+}
